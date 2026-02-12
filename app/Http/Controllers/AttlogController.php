00116b85323a820f96bc50fefeba3b845b1256d3
Soporte ossc <soporte@OSSCmexico.onmicrosoft.com>
2026-02-12 10:47:34 -0600

    creacion de tabla attlog y filtro para mostrar solo registros no procesados

--- app/Http/Controllers/AttlogController.php
+++ app/Http/Controllers/AttlogController.php
@@ -10,7 +10,7 @@ class AttlogController extends Controller
     public function index()
     {
         // 1. Obtener los registros crudos desde finger_log
-        $logs = DB::table('finger_log')->select('id', 'data')->get();
+        $logs = DB::table('finger_log')->select('id', 'data', 'url')->get();
 
         $attlogs = [];
         $deleted = DB::table('attlog')->delete();
@@ -27,31 +27,47 @@ class AttlogController extends Controller
                 if (preg_match('/^(FP|FINGERTMP|OPERLOG|OPLOG|~DeviceName)/i', $linea)) continue;
 
                 // 4. Separar por espacios o tabs
+                $url = json_decode($log->url);
+
+                $sn = $url->SN;
                 $cols = preg_split('/\s+/', $linea);
 
                 // Validar que tenga al menos 3 columnas
                 if (count($cols) < 3) continue;
 
-                $attlogs[] = [
-                    'id'          => $log->id,
-                    'employee_id' => $cols[0],
-                    'fecha'       => $cols[1],
-                    'hora'        => $cols[2],
-                    'status1'     => $cols[3] ?? null,
-                    'status2'     => $cols[4] ?? null,
-                    'status3'     => $cols[5] ?? null,
-                    'status4'     => $cols[6] ?? null,
-                    'status5'     => $cols[7] ?? null,
-                ];
+                $attendances = DB::table('attendances')
+                    ->where('employee_id', '=', $cols[0])
+                    ->where('timestamp', '=', new DateTime($cols[1].' '.$cols[2]))
+                    ->get();
 
-                $q['employee_id'] = $cols[0];
-                $q['timestamp'] = new DateTime($cols[1].' '.$cols[2]);
-                $q['status1'] = $cols[3];
-                $q['status2'] = $cols[4];
-                $q['status3'] = $cols[5];
-                $q['status4'] = $cols[6];
-                $q['status5'] = $cols[7];
-                DB::table('attlog')->insert($q);
+                
+                if($attendances->isEmpty()) {
+                    $attlogs[] = [
+                        'id'          => $log->id,
+                        'employee_id' => $cols[0],
+                        'fecha'       => $cols[1],
+                        'hora'        => $cols[2],
+                        'status1'     => $cols[3] ?? null,
+                        'status2'     => $cols[4] ?? null,
+                        'status3'     => $cols[5] ?? null,
+                        'status4'     => $cols[6] ?? null,
+                        'status5'     => $cols[7] ?? null,
+                    ];
+
+                    $q['sn'] = $url->SN;
+                    $q['table'] = 'ATTLOGR';
+                    $q['stamp'] = $url->Stamp;
+                    $q['employee_id'] = $cols[0];
+                    $q['timestamp'] = new DateTime($cols[1].' '.$cols[2]);
+                    $q['status1'] = $cols[3];
+                    $q['status2'] = $cols[4];
+                    $q['status3'] = $cols[5];
+                    $q['status4'] = $cols[6];
+                    $q['status5'] = $cols[7];
+                    $q['created_at'] = now();
+                    $q['updated_at'] = now();
+                    DB::table('attlog')->insert($q);
+                }
             }
         }
 
